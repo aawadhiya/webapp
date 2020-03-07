d010acae91e48f0b93260935cf15330317b881de
aawad <aawad@ubuntu.ubuntu-domain>
2020-03-07 17:41:11 -0500

    Assignment7

--- codedeploy/applicationstart.sh
+++ codedeploy/applicationstart.sh
@@ -5,11 +5,11 @@ sudo rm -r node_modules
 sudo rm -r logs
 sudo rm app.log
 npm install
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-config.json -s
 
 
-nohup node server.js >> app.log 2>&1 &
+
+nohup node app.js >> app.log 2>&1 &
 
 echo "starting application on 3001"
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-config.json -s
+

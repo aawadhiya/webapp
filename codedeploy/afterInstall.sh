4c885d54f26fa43dec6199894929fb973bfddb96
aawad <aawad@ubuntu.ubuntu-domain>
2020-03-06 22:21:02 -0500

    Assignment7

---
+++ codedeploy/afterInstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo chown centos /home/ubuntu/webapp/
+
+cd ~
+FILE=.env
+if test -f "$FILE"; then
+    echo "$FILE exist"
+    sudo cp /home/centos/.env /home/ubuntu/webapp/
+fi
+
+if ! test -f "$FILE"; then
+    echo "$FILE does not exist"
+fi
+
+# sudo systemctl stop tomcat.service
+
+# sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
+
+# sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
+
+# # cleanup log files
+# sudo rm -rf /opt/tomcat/logs/catalina*
+# sudo rm -rf /opt/tomcat/logs/*.log
+# sudo rm -rf /opt/tomcat/logs/*.txt
---
+++ codedeploy/beforeInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "ankit"
+echo $PWD

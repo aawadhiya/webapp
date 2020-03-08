08f2afc0fbef41597f8aabc2155ec5dbabb3bc19
aawad <aawad@ubuntu.ubuntu-domain>
2020-03-07 23:31:00 -0500

    assignment7

--- codedeploy/applicationstop.sh
+++ codedeploy/applicationstop.sh
@@ -1,5 +1,5 @@
 echo "getting the process id"
-PID=`ps -eaf | grep "node server.js" | grep -v grep | awk '{print $2}'`
+PID=`ps -eaf | grep "node app.js" | grep -v grep | awk '{print $2}'`
 echo "process id not empty ? $PID"
 if [[ "" !=  "$PID" ]]; then
   echo "Found node application running on $PID"
